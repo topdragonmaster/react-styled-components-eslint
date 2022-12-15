b1d855b8e736c8845826371ec09e6da27259691a
smart-dev30 <sengineer330@gmail.com>
2022-12-14 16:38:16 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import styled, { ThemeProvider } from 'styled-components'
+import { color } from 'styled-system'
+import styledMap from 'styled-map'
+
+const theme = {
+  colors: {
+    primary: 'blue',
+  },
+}
+
+const Item = styled.div`
+  color: ${(props) => props.theme.colors.primary};
+`
+const Box = styled.div`
+  padding: 10px;
+  ${color}
+`
+
+const buttonColor = styledMap`
+  primary: #0c0;
+  warning: #c00;
+  info: #0cc;
+  default: #ccc;
+`
+
+const Button = styled.button`
+  color: ${buttonColor};
+  border: 2px solid ${buttonColor};
+  font-size: ${styledMap`
+    large: 32px;
+    small: 8px;
+    medium: 18px;
+    default: 16px;
+  `};
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ThemeProvider theme={theme}>
+      <>
+        <Box color="#fff" bg="tomato">
+          Tomato
+        </Box>
+        <Box color="white" bg="primary">
+          Tomato
+        </Box>
+        <Item>Item 1</Item>
+        <Button primary large>
+          Submit
+        </Button>
+      </>
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
